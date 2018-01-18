91470f1801c0503782395df4c94f161e63015cec
Mikko Välimäki <mkko1373@gmail.com>
2018-01-18 18:23:19 +0100

    Make it a framework

---
+++ DrawerView/DrawerView.h
@@ -0,0 +1,19 @@
+//
+//  DrawerView.h
+//  DrawerView
+//
+//  Created by Mikko Välimäki on 18/01/2018.
+//  Copyright © 2018 Mikko Välimäki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrawerView.
+FOUNDATION_EXPORT double DrawerViewVersionNumber;
+
+//! Project version string for DrawerView.
+FOUNDATION_EXPORT const unsigned char DrawerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawerView/PublicHeader.h>
+
+
--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import DrawerView
 
 class ViewController: UIViewController {
 
